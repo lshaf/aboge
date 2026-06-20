95e8748a07169e14cd89a50f32030b0fa4808d11
lshaf <lshaf@users.noreply.github.com>
2026-06-20 21:47:06 +0700

    🔤 Self-host Fraunces & Plus Jakarta Sans for offline/installed use

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// Self-hosted fonts (bundled + precached so they work offline / when installed)
+import '@fontsource-variable/fraunces'
+import '@fontsource-variable/plus-jakarta-sans'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
